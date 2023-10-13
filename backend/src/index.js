bf2591b5248d8e77356ad21f510257708ea7a957
Sam Cutter <samuelcutter27@gmail.com>
2023-10-13 23:17:29 +0100

    Implemented functionality to get product price, also created options for product condition

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,3 +17,55 @@ function extract_product_id(product_url_string) {
     return "Invalid product URL.";
   }
 }
+
+const product_condition_options = {
+  Fair: 12,
+  Good: 11,
+  Excellent: 10,
+};
+
+async function get_product_price(product_id, condition) {
+  const product_request_url = `https://www.backmarket.co.uk/bm/product/${product_id}/pickers`;
+
+  const product_request_headers = new Headers({
+    "Accept-Language": "en-gb",
+  });
+
+  const product_request_options = {
+    method: "GET",
+    headers: product_request_headers,
+  };
+
+  try {
+    const product_request_response = await fetch(
+      product_request_url,
+      product_request_options
+    );
+
+    if (!product_request_response.ok) {
+      throw new Error(
+        `Response status code was not 200: ${product_request_response.status}.`
+      );
+    }
+
+    const product_data = await product_request_response.json();
+
+    const product_grade_picker = product_data.pickers.find(
+      (picker) => picker.id === "grades"
+    );
+
+    if (!product_grade_picker) {
+      throw new Error("Condition picker not found within pickers array.");
+    }
+
+    const product_item = product_grade_picker.items.find(
+      (item) => item.grade.value === condition
+    );
+
+    if (product_item) {
+      return product_item.price.amount;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
