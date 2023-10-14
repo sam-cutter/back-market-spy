88a8c404d3898b3f4dc87a35549a35f283839686
Sam Cutter <samuelcutter27@gmail.com>
2023-10-14 10:47:55 +0100

    Created init_pocketbase function

---
+++ backend/src/pocketbase.js
@@ -0,0 +1,13 @@
+import PocketBase from "pocketbase";
+import "dotenv/config";
+
+export async function init_pocketbase() {
+  const pb = new PocketBase("http://127.0.0.1:8090");
+
+  await pb.admins.authWithPassword(
+    process.env.POCKETBASE_ADMIN_EMAIL,
+    process.env.POCKETBASE_ADMIN_PASSWORD
+  );
+
+  return pb;
+}
