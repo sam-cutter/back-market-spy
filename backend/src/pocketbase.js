d473da3189c7cedeaea8cdb321bf3dc6fc71b91b
Sam Cutter <samuelcutter27@gmail.com>
2023-10-14 11:20:27 +0100

    Created add_tracked_product function

--- backend/src/pocketbase.js
+++ backend/src/pocketbase.js
@@ -1,5 +1,6 @@
 import PocketBase from "pocketbase";
 import "dotenv/config";
+import { get_product_slug } from "./back_market.js";
 
 export async function init_pocketbase() {
   const pb = new PocketBase("http://127.0.0.1:8090");
@@ -22,4 +23,19 @@ export async function get_tracked_products() {
   });
 
   return tracked_products;
+}
+
+export async function add_tracked_product(product_id) {
+  const pb = await init_pocketbase();
+
+  const product_data = {
+    pb_uuid: product_id,
+    slug: await get_product_slug(product_id),
+  };
+
+  const tracked_product_record = await pb
+    .collection("tracked_products")
+    .create(product_data);
+
+  return tracked_product_record.id;
 }
