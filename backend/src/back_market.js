9a039d2f80eb565058398f5fbe1cca74fce38420
Sam Cutter <samuelcutter27@gmail.com>
2023-10-16 20:40:43 +0100

    Created function to evaluate product URL

--- backend/src/back_market.js
+++ backend/src/back_market.js
@@ -1,45 +1,122 @@
-export function extract_product_bm_uuid(product_url_string) {
-  try {
-    const product_url = new URL(product_url_string);
-    const product_url_path_segments = product_url.pathname.split("/");
-
-    for (let i = 0; i < product_url_path_segments.length; i++) {
-      if (
-        product_url_path_segments[i] == "p" &&
-        i + 2 < product_url_path_segments.length
-      ) {
-        return product_url_path_segments[i + 2];
-      }
-    }
-
-    return "Product ID not found in product URL.";
-  } catch (error) {
-    return "Invalid product URL.";
-  }
-}
-
-export function extract_product_condition(product_url_string) {
-  try {
-    const product_url = new URL(product_url_string);
-    const product_url_hash = product_url.hash;
-
-    const product_url_condition_match = product_url_hash.match(/#l=(\d+)/);
-
-    if (product_url_condition_match && product_url_condition_match[1]) {
-      return parseInt(product_url_condition_match[1]);
-    } else {
-      return product_condition_options.Fair();
-    }
-  } catch (error) {
-    return "Invalid product URL.";
-  }
-}
+import { UserInputError } from "./errors.js";
 
 export const product_condition_options = {
   Fair: 12,
   Good: 11,
   Excellent: 10,
 };
+
+export function evaluate_product_bm_url_string(product_url_string) {
+  const VALID_HOSTNAMES = [
+    "www.backmarket.co.uk",
+    "www.backmarket.fr",
+    "www.backmarket.com",
+  ];
+
+  try {
+    const product_url = new URL(product_url_string);
+
+    // ---------------------------------------------------------------------------------- //
+    // If the hostname of the input product URL is not a valid hostname, URL is invalid.
+    if (!VALID_HOSTNAMES.includes(product_url.hostname)) {
+      return {
+        valid: false,
+        reason: "Invalid product URL hostname.",
+      };
+    }
+    // ---------------------------------------------------------------------------------- //
+
+    const product_url_path_segments = product_url.pathname
+      .split("/")
+      .filter((segment) => {
+        return segment !== "";
+      });
+
+    // ---------------------------------------------------------------------------------- //
+    // If the pathname of the input product URL does not match the pattern:
+    // /:locale/p/:slug/:uuid
+    // URL is invalid.
+    if (
+      product_url_path_segments.length !== 4 ||
+      product_url_path_segments[1] !== "p"
+    ) {
+      return {
+        valid: false,
+        reason: `Invalid product URL pattern.`,
+      };
+    }
+    // ---------------------------------------------------------------------------------- //
+
+    return {
+      valid: true,
+      path_segments: product_url_path_segments,
+      hash: product_url.hash,
+    };
+  } catch {
+    return {
+      valid: false,
+      reason: "Unable to interpret product URL.",
+    };
+  }
+}
+
+export function extract_product_bm_uuid(product_url_string) {
+  const product_url_evaluation =
+    evaluate_product_bm_url_string(product_url_string);
+
+  // ---------------------------------------------------------------------------------- //
+  // If the evaluation of the product URL returns that the URL is invalid, throw an error.
+  if (product_url_evaluation.valid === false) {
+    throw new UserInputError(
+      `Invalid product URL: ${product_url_evaluation.reason}`
+    );
+  }
+  // ---------------------------------------------------------------------------------- //
+
+  const product_url_path_segments = product_url_evaluation.path_segments;
+
+  const product_bm_uuid = product_url_path_segments[3];
+
+  const uuid_regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+  // ---------------------------------------------------------------------------------- //
+  // If the UUID does not match the common UUID regex, throw an error.
+  if (!uuid_regex.test(product_bm_uuid)) {
+    throw new UserInputError("Invalid product URL: Invalid product UUID.");
+  }
+  // ---------------------------------------------------------------------------------- //
+
+  return product_bm_uuid;
+}
+
+export function extract_product_condition(product_url_string) {
+  const product_url_evaluation =
+    evaluate_product_bm_url_string(product_url_string);
+
+  // ---------------------------------------------------------------------------------- //
+  // If the evaluation of the product URL returns that the URL is invalid, throw an error.
+  if (product_url_evaluation.valid === false) {
+    throw new UserInputError(
+      `Invalid product URL: ${product_url_evaluation.reason}`
+    );
+  }
+  // ---------------------------------------------------------------------------------- //
+
+  const product_url_hash = product_url_evaluation.hash;
+
+  if (!product_url_hash) {
+    return product_condition_options.Fair;
+  }
+
+  const product_url_condition_match = product_url_hash.match(/#l=(\d+)/);
+
+  if (product_url_condition_match && product_url_condition_match[1]) {
+    return parseInt(product_url_condition_match[1]);
+  } else {
+    return product_condition_options.Fair;
+  }
+}
 
 export async function get_product_data(product_bm_uuid, condition) {
   const product_request_url = `https://www.backmarket.co.uk/bm/product/${product_bm_uuid}/pickers`;
---
+++ backend/src/errors.js
@@ -0,0 +1,5 @@
+export class UserInputError extends Error {
+  constructor(msg) {
+    super(msg);
+  }
+}
