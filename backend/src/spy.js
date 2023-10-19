51599c9043b4c6c8386ae9e42812a3b23b251889
Sam Cutter <samuelcutter27@gmail.com>
2023-10-19 18:56:50 +0100

    Renamed all condition variables to grade

--- backend/src/spy.js
+++ backend/src/spy.js
@@ -5,19 +5,12 @@ export async function generate_product_snapshot(
   product_bm_uuid,
   product_record_id
 ) {
-  for (
-    let product_condition = 10;
-    product_condition <= 12;
-    product_condition++
-  ) {
-    const product_data = await get_product_data(
-      product_bm_uuid,
-      product_condition
-    );
+  for (let product_grade = 10; product_grade <= 12; product_grade++) {
+    const product_data = await get_product_data(product_bm_uuid, product_grade);
 
     await add_product_snapshot(
       product_record_id,
-      product_condition,
+      product_grade,
       product_data.in_stock,
       product_data.price
     );
