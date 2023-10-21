d85ab2087bf5364c1f1b1bbbe16820acd1a6e529
Sam Cutter <samuelcutter27@gmail.com>
2023-10-21 15:56:26 +0100

    Commented code, removed slug retrieving functionality

--- backend/src/back_market.js
+++ backend/src/back_market.js
@@ -96,27 +96,37 @@ export async function get_product_data(product_bm_uuid) {
       headers: { "Accept-Language": "en-gb" },
     });
 
+    // ---------------------------------------------------------------------------------- //
+    // If the response code is not 200, something has gone wrong, so throw an error.
     if (!product_request_response.ok) {
       throw new Error(
         `Back Market API returned a non-200 response code: ${product_request_response.status}`
       );
     }
+    // ---------------------------------------------------------------------------------- //
 
     const product_data = await product_request_response.json();
     const product_grade_picker = product_data.pickers.find(
       (picker) => picker.id && picker.id === "grades"
     );
 
+    // ---------------------------------------------------------------------------------- //
+    // If we are unable to find the product grade picker
     if (!product_grade_picker) {
       throw new Error("Grade picker not found within product pickers.");
     }
+    // ---------------------------------------------------------------------------------- //
 
+    // ---------------------------------------------------------------------------------- //
+    // Map the product data into an array. When the product is unavailable,
+    // it won't have a price, so leave as undefined
     const product_items = product_grade_picker.items.map(
       (product_item, index) => {
         const grade = 12 - index;
         const price_gbp = product_item.available
           ? product_item.price.amount
           : undefined;
+
         return {
           grade: grade,
           available: product_item.available,
@@ -124,13 +134,7 @@ export async function get_product_data(product_bm_uuid) {
         };
       }
     );
-
-    const product_item_with_slug = product_items.find(
-      (item) => item.available && item.link
-    );
-    const product_slug = product_item_with_slug
-      ? product_item_with_slug.link.params.slugV2
-      : undefined;
+    // ---------------------------------------------------------------------------------- //
 
     return {
       success: {
@@ -138,7 +142,6 @@ export async function get_product_data(product_bm_uuid) {
         reason: "",
       },
       data: {
-        slug: product_slug,
         items: product_items,
       },
     };
@@ -151,9 +154,10 @@ export async function get_product_data(product_bm_uuid) {
         reason: error.message,
       },
       data: {
-        slug: "",
         items: [],
       },
     };
   }
 }
+
+console.log(await get_product_data("0b162fa7-1929-4e6c-9ee2-39921d79a843"));
